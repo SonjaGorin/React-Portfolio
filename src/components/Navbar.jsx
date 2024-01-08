804e78e27bcce61f783fb60f59dd409667e9231e
Sonja <sonjaljatkovic@gmail.com>
2024-01-08 00:14:59 -0500

    fixed the issue in vite.config.js

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -21,7 +21,24 @@ function Navbar() {
                     Contact
                 </Link>
             </li>
-            
+            <li>
+                <Link
+                    to="/Portfolio"
+                    className={currentPage === '/Portfolio' ? 'nav-link active' : 'nav-link'}
+                >
+                    Portfolio
+                </Link>
+            </li>
+            <li>
+                <Link
+                    to="/Resume"
+                    className={currentPage === '/Resume' ? 'nav-link active' : 'nav-link'}
+                >
+                    Resume
+                </Link>
+            </li>
         </ul>
     )
 }
+
+export default Navbar;
