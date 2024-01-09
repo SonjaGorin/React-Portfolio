bfdbf7b6a793fbab712c5459db1c0c35943d5e23
Sonja <sonjaljatkovic@gmail.com>
2024-01-09 16:05:30 -0500

    added button to download resume

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -1,7 +1,17 @@
+import React from "react";
+import ResumeFile from "../../images/resume.pdf";
+
 export default function Resume() {
     return (
         <div>
             <h1>Resume</h1>
+            <h2>Click on the button to download my resume</h2>
+            <a href={ResumeFile} download>
+                <button>
+                    Resume
+                </button>
+            </a>
         </div>
+        
     );
 }
