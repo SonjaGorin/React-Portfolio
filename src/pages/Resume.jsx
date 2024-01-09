b3293c39b0ee16bcf9a173f1825a3a4d48e450a3
Sonja <sonjaljatkovic@gmail.com>
2024-01-09 16:16:59 -0500

    added a list of proficiencies to the resume page

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -11,7 +11,30 @@ export default function Resume() {
                     Resume
                 </button>
             </a>
+            <h2>Front-end Proficiencies</h2>
+            <ul>
+                <li>HTML</li>
+                <li>CSS</li>
+                <li>JavaScript</li>
+                <li>JQuery</li>
+                <li>Responsive Design</li>
+                <li>React</li>
+                <li>Bootstrap</li>
+            </ul>
+            <h2>Back-end Proficiencies</h2>
+            <ul>
+                <li>APIs</li>
+                <li>NodeJS</li>
+                <li>Express</li>
+                <li>Model View Controller (MVC)</li>
+                <li>MySQL</li>
+                <li>Sequelize</li>
+                <li>Progressive Web Applications (PWA)</li>
+                <li>MongoDB</li>
+                <li>Mongoose</li>
+                <li>REST</li>
+                <li>GraphQL</li>
+            </ul>
         </div>
-        
     );
 }
