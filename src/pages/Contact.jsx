c3a10be2f2b6daad6040e30afd011c56d973ab77
Sonja <sonjaljatkovic@gmail.com>
2024-01-08 21:03:57 -0500

    added vlidations for empty fields

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { validateEmail } from "../utils/validateEmail";
 
 function Form() {
-    const [name, setName] = useState("");
+    const [fullName, setName] = useState("");
     const [email, setEmail] = useState("");
     const [message, setMessage] = useState("");
     const [errorMessage, setErrorMessage] = useState("");
@@ -11,9 +11,13 @@ function Form() {
 
         const { name, value } = e.target;
 
+        if (value === "") {
+            setErrorMessage(`The ${name} field is required.`);
+        }
+
         if (name === "name") {
             return setName(value)
-        } else if (name === "email") {
+        }  else if (name === "email") {
             return setEmail(value)
         } else {
             return setMessage(value)
@@ -23,12 +27,17 @@ function Form() {
     const handleFormSubmit = (e) => {
         e.preventDefault();
 
+        const form = e.target
+
+        if (form.name.value === "" || form.email.value === "" || form.message.value === "") {
+            setErrorMessage("Please fill up all fields.");
+            return;
+        } 
+        
         if (!validateEmail(email)) {
             setErrorMessage("Please enter a valid email address.");
             return;
-        } else {
-            setErrorMessage("");
-        };
+        }
 
         setName("");
         setEmail("");
@@ -41,30 +50,41 @@ function Form() {
                 Contact
             </h1>
             <form className="form" onSubmit={handleFormSubmit}>
-                <input
-                value={name}
-                name="name"
-                onChange={handleInputChange}
-                type="text"
-                placeholder="first and last name"
-                />
-                <input
-                value={email}
-                name="email"
-                onChange={handleInputChange}
-                type="text"
-                placeholder="email"
-                />
-                <input
-                value={message}
-                name="message"
-                onChange={handleInputChange}
-                type="text"
-                placeholder="message"
-                />
-                <button type="submit">
-                    Submit
-                </button>
+                <div>
+                    <label>Name</label>
+                    <input
+                    value={fullName}
+                    name="name"
+                    onChange={handleInputChange}
+                    type="text"
+                    placeholder="first and last name"
+                    />
+                </div>
+                <div>
+                    <label>Email</label>
+                    <input
+                    value={email}
+                    name="email"
+                    onChange={handleInputChange}
+                    type="text"
+                    placeholder="email"
+                    />
+                </div>
+                <div>
+                    <label>Message</label>
+                    <input
+                    value={message}
+                    name="message"
+                    onChange={handleInputChange}
+                    type="text"
+                    placeholder="message"
+                    />
+                </div>
+                <div>
+                    <button type="submit">
+                        Submit
+                    </button>
+                </div>
             </form>
             {errorMessage && (
                 <div>
