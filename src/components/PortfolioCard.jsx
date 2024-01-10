1e73888b6495575bc5d00d458d2248e592bdbb01
Sonja <sonjaljatkovic@gmail.com>
2024-01-09 20:32:51 -0500

    created portfolio card and updated portfolio page

--- src/components/PortfolioCard.jsx
+++ src/components/PortfolioCard.jsx
@@ -1,11 +1,12 @@
-function PortfolioCard() {
+import React from "react";
+
+export default function PortfolioCard(props) {
+    console.log(props);
     return(
         <div>
-            <div>
-                
-            </div>
+            <img src={props.img}></img>
+            <a href={props.app}>PLACEHOLDER</a>
+            <a href={props.git}>placeholder</a>
         </div>
     );
 }
-
-export default PortfolioCard;
--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -1,15 +1,23 @@
 import PortfolioCard from "../components/PortfolioCard";
+import CalculatorImage from "../../images/calculator.jpg";
+import JavaScriptImage from "../../images/JavaScript.jpg";
+import LaptopImage from "../../images/laptop.jpg";
+import LedWallImage from "../../images/led-wall.png";
+import PastelPuzzlesImage from "../../images/pastel-puzzles.jpg";
+import RunBuddyImage from "../../images/run-buddy.png";
+
+
 
 export default function Portfolio() {
     return (
         <div>
             <h1>Portfolio</h1>
-            <PortfolioCard />
-            <PortfolioCard />
-            <PortfolioCard />
-            <PortfolioCard />
-            <PortfolioCard />
-            <PortfolioCard />
+            <PortfolioCard img={CalculatorImage} app={"https://sonjagorin.github.io/Code-Quiz/"} git={"https://github.com/SonjaGorin"}/>
+            <PortfolioCard img={JavaScriptImage}/>
+            <PortfolioCard img={LaptopImage}/>
+            <PortfolioCard img={LedWallImage}/>
+            <PortfolioCard img={PastelPuzzlesImage}/>
+            <PortfolioCard img={RunBuddyImage}/>
         </div>
     );
 }
