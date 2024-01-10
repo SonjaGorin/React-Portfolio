c0bebe182a7167df500be0566619447aaf68f850
Sonja <sonjaljatkovic@gmail.com>
2024-01-10 17:10:49 -0500

    styled AboutMe page

--- src/pages/AboutMePage/AboutMe.jsx
+++ src/pages/AboutMePage/AboutMe.jsx
@@ -3,16 +3,16 @@ import "./AboutMe.css"
 
 export default function AboutMe() {
     return (
-        <div>
+        <div className="about-page">
             <h1>About Me</h1>
             <div>
                 <img className="avatar" src={AvatarImage} alt="Sonja Gorin avatar photo."></img>
             </div>
             <p>
-                Hello! I'm a full stack software developer with a background in education. 
-                I have a certificate in full stack development from Carleton University bootcamp.
-                My two Bachelor's degrees and experience working as a teacher gave me very valuable knowledge and ability to learn quickly and efficiently,
-                which have shown to be necessary skills throughout my course. I am very excited to continue utilizing my newly obtained 
+                Hello! I'm a full stack software developer with a background in education. <br></br>
+                I have a certificate in full stack development from Carleton University bootcamp.<br></br>
+                My two Bachelor's degrees and experience working as a teacher gave me very valuable knowledge and ability to learn quickly and efficiently, <br></br>
+                which have shown to be necessary skills throughout my course.<br></br> I am very excited to continue utilizing my newly obtained 
                 expertize as a MERN stack developer.
             </p>
         </div>
