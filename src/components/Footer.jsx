2007bda1c56beb86d98610f8ea0a728d04ddc15a
Sonja <sonjaljatkovic@gmail.com>
2024-01-10 11:12:58 -0500

    updated footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,85 +1,54 @@
-import React from "react";
-
 import {
   MDBFooter,
   MDBContainer,
-  MDBCol,
-  MDBRow,
   MDBIcon,
   MDBBtn
 } from 'mdb-react-ui-kit';
-
-// import { Ripple, initMDB } from "mdb-ui-kit";
-// initMDB({ Ripple });
-// import GitHubImage from "../../images/GitHub-logo-transparent.png";
-// import LinkedInImage from "../../images/LinkedIn-logo-initials.png";
-// import TwitterImage from "../../images/twitter-logo-transparent.png";
-
-
-    //   <footer className="footer">
-    //     <h1>Footer</h1>
-    //     <div className="pop-on-hover">
-    //         <a href="https://github.com/SonjaGorin" target="_blank">
-    //             <img src={GitHubImage} alt="GitHub logo."></img>
-    //         </a>
-    //     </div>
-    //     <div>
-    //         <a href="https://ca.linkedin.com/" target="_blank">
-    //             <img src={LinkedInImage} alt="Linked In logo."></img>
-    //         </a>
-    //     </div>
-    //     <div>
-    //         <a href="https://twitter.com/?lang=en" target="_blank">
-    //             <img src={TwitterImage} alt="Twitter/X logo."></img>
-    //         </a>
-    //     </div>
-    //   </footer>
-
 
 function Footer() {
     return (
         <MDBFooter className='text-center text-white' style={{ backgroundColor: '#f1f1f1' }}>
             <MDBContainer className='pt-4'>
-                <section className='mb-4'>
+                <section>
                     <MDBBtn
                         rippleColor="dark"
                         color='link'
-                        floating
-                        size="lg"
-                        className='text-dark m-1'
-                        href='https://twitter.com/?lang=en'
-                        role='button'
-                    >
-                        <MDBIcon fab className='fa-twitter' />
-                    </MDBBtn>
-                    <MDBBtn
-                        rippleColor="dark"
-                        color='link'
-                        floating
-                        size="lg"
-                        className='text-dark m-1'
-                        href='https://ca.linkedin.com/'
-                        role='button'
-                    >
-                        <MDBIcon fab className='fa-linkedin' />
-                    </MDBBtn>
-                    <MDBBtn
-                        rippleColor="dark"
-                        color='link'
+                        target="_blank"
                         floating
                         size="lg"
                         className='text-dark m-1'
                         href='https://github.com/SonjaGorin'
                         role='button'
                     >
-                        <MDBIcon fab className='fa-github' />
+                        <MDBIcon fab className='fa-github' size="2x"/>
+                    </MDBBtn>
+                    <MDBBtn
+                        rippleColor="dark"
+                        color='link'
+                        target="_blank"
+                        floating
+                        size="lg"
+                        className='text-dark m-1'
+                        href='https://ca.linkedin.com/'
+                        role='button'
+                    >
+                        <MDBIcon fab className='fa-linkedin' size="2x"/>
+                    </MDBBtn>
+                    <MDBBtn
+                        rippleColor="dark"
+                        color='link'
+                        target="_blank"
+                        floating
+                        size="lg"
+                        className='text-dark m-1'
+                        href='https://twitter.com/?lang=en'
+                        role='button'
+                    >
+                        <MDBIcon fab className='fa-twitter' size="2x"/>
                     </MDBBtn>
                 </section>
-                
             </MDBContainer>
-            
         </MDBFooter>
-        
     );
 }
   
