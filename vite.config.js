266e81e48f02177a7a92542edaf94efd669d4b4a
Sonja <sonjaljatkovic@gmail.com>
2024-01-11 18:07:39 -0500

    imported Header.css to the vite.config file

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import HeaderCss from "./src/components/Header/Header.css"
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,7 +11,7 @@ export default defineConfig({
   },
   build: {
     rollupOptions: {
-      external: ['Header.css'],
+      external: [HeaderCss],
     }
   }
 });
