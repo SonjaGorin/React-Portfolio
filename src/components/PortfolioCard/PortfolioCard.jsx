860bb1b0ed6a9f0367e7204fd996e5c8538d3fda
Sonja <sonjaljatkovic@gmail.com>
2024-01-10 22:37:13 -0500

    styled portfolio card links

--- src/components/PortfolioCard/PortfolioCard.jsx
+++ src/components/PortfolioCard/PortfolioCard.jsx
@@ -4,8 +4,10 @@ export default function PortfolioCard(props) {
     return(
         <div className="portfolio-card">
             <img className="portfolio-image" src={props.img}></img>
-            <a href={props.app}>PLACEHOLDER</a>
-            <a href={props.git}>placeholder</a>
+            <div className="project-links">
+                <a href={props.app} className="project-anchor">{props.project}</a>
+                <a href={props.git} className="bi bi-github fa-2x git-anchor"></a>
+            </div>
         </div>
     );
 }
--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -15,16 +15,16 @@ export default function Portfolio() {
             <h1>Portfolio</h1>
             <div>
                 <div className="two-cards">
-                    <PortfolioCard img={WorkDayScheduler} app={"https://sonjagorin.github.io/Work-Day-Scheduler/"} git={"https://github.com/SonjaGorin/Work-Day-Scheduler"}/>
-                    <PortfolioCard img={WeatherDashboard} app={"https://sonjagorin.github.io/Weather-Dashboard/"} git={"https://github.com/SonjaGorin/Weather-Dashboard"}/>
+                    <PortfolioCard project={"Work Day Scheduler"} img={WorkDayScheduler} app={"https://sonjagorin.github.io/Work-Day-Scheduler/"} git={"https://github.com/SonjaGorin/Work-Day-Scheduler"}/>
+                    <PortfolioCard project={"Weather Dashboard"} img={WeatherDashboard} app={"https://sonjagorin.github.io/Weather-Dashboard/"} git={"https://github.com/SonjaGorin/Weather-Dashboard"}/>
                 </div>
                 <div className="two-cards">
-                    <PortfolioCard img={Cravings} app={"https://craving-510bbff9dcfd.herokuapp.com/"} git={"https://github.com/SonjaGorin/Cravings"}/>
-                    <PortfolioCard img={TechBlog} app={"https://technology-blog-sg-b51f56d78026.herokuapp.com/"} git={"https://github.com/SonjaGorin/Tech-Blog"}/>
+                    <PortfolioCard project={"Cravings"} img={Cravings} app={"https://craving-510bbff9dcfd.herokuapp.com/"} git={"https://github.com/SonjaGorin/Cravings"}/>
+                    <PortfolioCard project={"Tech Blog"} img={TechBlog} app={"https://technology-blog-sg-b51f56d78026.herokuapp.com/"} git={"https://github.com/SonjaGorin/Tech-Blog"}/>
                 </div>
                 <div className="two-cards">
-                    <PortfolioCard img={NoteTaker} app={"https://murmuring-beyond-80827-82186790f589.herokuapp.com/"} git={"https://github.com/SonjaGorin/Note-Taker?tab=readme-ov-file"}/>
-                    <PortfolioCard img={PasswordGenerator} app={"https://sonjagorin.github.io/Password-Generator/"} git={"https://github.com/SonjaGorin/Password-Generator"}/>
+                    <PortfolioCard project={"Note Taker"} img={NoteTaker} app={"https://murmuring-beyond-80827-82186790f589.herokuapp.com/"} git={"https://github.com/SonjaGorin/Note-Taker?tab=readme-ov-file"}/>
+                    <PortfolioCard project={"Password Generator"} img={PasswordGenerator} app={"https://sonjagorin.github.io/Password-Generator/"} git={"https://github.com/SonjaGorin/Password-Generator"}/>
                 </div>
             </div>
         </div>
