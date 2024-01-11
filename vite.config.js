28776c06e0be56cc482eb46911d591d4868e6b6b
Sonja <sonjaljatkovic@gmail.com>
2024-01-11 18:10:11 -0500

    updated vite.config file

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import HeaderCss from "./src/components/Header/Header.css"
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,9 +8,4 @@ export default defineConfig({
     port: 3000,
     open: true,
   },
-  build: {
-    rollupOptions: {
-      external: [HeaderCss],
-    }
-  }
 });
