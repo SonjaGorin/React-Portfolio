2b6af6c073462dc7ea57ef87bdb1e99f44521e5c
Sonja <sonjaljatkovic@gmail.com>
2024-01-11 13:46:45 -0500

    added coments to main and App

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
 import image from "../images/background.webp";
 
-
+// App will render header, footer and the main part using Outlet from React
 function App() {
   return (
     <div style={{ 
--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,20 @@
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
+// importing all files needed for styling
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import 'bootstrap-css-only/css/bootstrap.css';
 import "bootstrap/dist/css/bootstrap.min.css";
 
-
+// importing all pages an App
 import App from "./App";
 import AboutMe from "./pages/AboutMePage/AboutMe";
 import Contact from "./pages/ContactPage/Contact";
 import Portfolio from "./pages/Portfolio";
 import Resume from "./pages/ResumePage/Resume";
 
+// creating routes for each page
 const router = createBrowserRouter([
   {
     path: "/",
