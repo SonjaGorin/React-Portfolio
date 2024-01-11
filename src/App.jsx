1586614c0fed70e4b83bb4d4838755b91d829d98
Sonja <sonjaljatkovic@gmail.com>
2024-01-10 23:46:50 -0500

    added the background image

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,20 @@
 import { Outlet } from "react-router-dom";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
+import image from "../images/background.webp";
 
 
 function App() {
   return (
-    <>
+    <div style={{ 
+      backgroundImage: `url(${image})`
+    }}>
       <Header />
       <main>
         <Outlet />
       </main>
       <Footer />
-    </>
+    </div>
   );
 }
 
